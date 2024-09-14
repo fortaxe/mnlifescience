a906076775cd3947ac018c2c5ed79225a9076d29
amarnath666 <amarnathdhumal2001@gmail.com>
2024-09-14 12:05:45 +0530

    commit

--- routes/scheduleCall.js
+++ routes/scheduleCall.js
@@ -5,13 +5,13 @@ import authMiddleware from '../middleware/auth.js';
 const router = express.Router();
 
 // Route to create a doctor schedule call
-router.post('/schedule/call/doctor', createDoctorScheduleCall);
+router.post('/schedule/call/doctor',authMiddleware('admin'), createDoctorScheduleCall);
 
 // Route to create a pharmacy schedule call
-router.post('/schedule/call/pharmacy', createPharmacyScheduleCall);
+router.post('/schedule/call/pharmacy',authMiddleware('admin'), createPharmacyScheduleCall);
 
 // Route to create a both schedule call
-router.post('/schedule/call/both', createBothScheduleCall);
+router.post('/schedule/call/both',authMiddleware('admin'), createBothScheduleCall);
 router.patch('/schedule/reschedule',authMiddleware('admin'), rescheduleScheduleCall);
 router.patch('/schedule/update-status',authMiddleware('admin'), updateScheduleCall);
 router.get('/schedule/today', authMiddleware('admin'), getTodaysScheduleCalls);
